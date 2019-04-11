869241f02a202333b3749c7a6df8debc6e07d2c9
Kid Rich Club .com <shieldeiei@gmail.com>
2019-04-11 14:58:08 +0700

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit',(event)=>{
     const location = search.value
 
     console.log(location)
-    fetch('http://localhost:3000/weather?address='+location).then((res)=>{
+    fetch('/weather?address='+location).then((res)=>{
     
         res.json().then((data)=>{
             if(data.error){
